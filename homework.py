b23fcf53a3f67cb9297e65b3c651b50d0a29541a
Ирина Бобина <bobin.ir495@gmail.com>
2020-12-23 13:00:17 +0300

    шгиршло

--- homework.py
+++ homework.py
@@ -30,7 +30,7 @@ def get_homework_statuses(current_timestamp):
     if current_timestamp is None:
         current_timestamp = int(time.time())
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    params = {'from_date': 0}
+    params = {'from_date': current_timestamp}
     #try:
     homework_statuses = requests.get(
         url=URL,
